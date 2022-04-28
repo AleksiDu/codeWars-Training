7df39c4e9e2ca0d1f474af5076aae6edf496ce66
AleksiDu <aleksi.duluzauri@gmail.com>
2022-04-28 12:17:05 +0400

    Convert a Number to a String

--- kataTraining.js
+++ kataTraining.js
@@ -12,10 +12,22 @@ console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); */
 
 //Remove First and Last Character
 
+/*
 function removeChar(str){
     return str.slice(1, -1);
    };
 
    console.log(removeChar("hello"));
+*/
+
+//Convert a Number to a String!
+
+function numberToString(num) {
+    return num.toString();
+  }
+
+  console.log(numberToString(9));
+  console.log(typeof(numberToString(9)));
+  
    
    
