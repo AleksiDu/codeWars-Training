ce5f9b6198b3d70709791d7a1324a1ffb97ef610
AleksiDu <aleksi.duluzauri@gmail.com>
2022-05-19 21:54:40 +0400

    init: Sum of positive

--- kyu8.js
+++ kyu8.js
@@ -1,4 +1,4 @@
-//Even or Odd
+// Even or Odd
 
 let evenOrODdd = (int) => {
     if (isNaN(int) || int === null || int === "" || int === undefined) {
@@ -11,6 +11,17 @@ let evenOrODdd = (int) => {
 };
 
 console.log(evenOrODdd(2));
+
+// Sum of positive
+let positiveSum = (arr) => {
+    let result = 0;
+    for(const element of arr) {
+        if (element > 0) result += element;
+    }
+    return result;
+}
+
+console.log(positiveSum([1,2,3,4,5]));
 
 
 
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,1 @@
-
-        return "Fire!";
+result
