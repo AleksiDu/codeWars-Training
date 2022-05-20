599ec545255306058c3e50c3654328d972830cc8
AleksiDu <aleksi.duluzauri@gmail.com>
2022-05-20 21:55:09 +0400

    init: String repeat

--- kyu8.js
+++ kyu8.js
@@ -15,13 +15,13 @@ console.log(evenOrODdd(2));
 // Sum of positive
 let positiveSum = (arr) => {
     let result = 0;
-    for(const element of arr) {
+    for (const element of arr) {
         if (element > 0) result += element;
     }
     return result;
 }
 
-console.log(positiveSum([1,2,3,4,5]));
+console.log(positiveSum([1, 2, 3, 4, 5]));
 
 // Return Negative
 
@@ -42,8 +42,21 @@ console.log(opposite(-3));
 //Convert bollean values to strings "Yes" or "No"
 
 let boolToWord = (bool) => {
-    if(bool === true) return "Yes";
-    if(bool === false) return "No";
+    if (bool === true) return "Yes";
+    if (bool === false) return "No";
 };
 
 console.log(boolToWord(true));
+
+//String repeat
+//My solution
+let repeatStr = (n, s) => {
+    let r = s;
+    for (let i = 0; i < n - 1; i++) { //Best Practice return s.repeat(n);
+        r = r + s;
+    }
+    return r;
+};
+
+console.log(repeatStr(5, '*'));
+
