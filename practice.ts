982225f58ee947403c31f4b8c623144f26b11551
AleksiDu <aleksi.duluzauri@gmail.com>
2023-03-06 10:24:20 +0400

    Multiply of 3 or 5

--- practice.ts
+++ practice.ts
@@ -18,3 +18,23 @@ export function multiply(a: number, b: number): number {
 }
 
 console.log(multiply(1, 1));
+
+// Multiples of 3 or 5
+console.log("Multiples of 3 or 5");
+
+export class Challenge {
+  static solution(number: number) {
+    let sum = 0;
+    if (number < 0) {
+      return 0;
+    }
+    for (let i = 0; i < number; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+}
+
+console.log(Challenge.solution(10));
